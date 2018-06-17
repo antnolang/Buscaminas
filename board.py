88954946262a5fccf0d4393965947c17ca53db26
Antonio Nolé <antnolangos@gmail.com>
2018-06-17 20:39:36 +0200

    Último retoque

--- board.py
+++ board.py
@@ -150,9 +150,9 @@ class Board():
         for sq in hidden:
             prob_X[(sq[0],sq[1])] = calcule_prob_X(self.variable_elimination, sq[0], sq[1], self.evidences)
             
-        return prob_X
+        # DEBUGGING: return prob_X
         # En caso de que haya dos valores máximos, devuelve el primero que encontró
-        # DEBUGGING: return max(prob_X.items(), key=operator.itemgetter(1))[0]
+        return max(prob_X.items(), key=operator.itemgetter(1))[0]
         
     def __get_hidden_squares__(self):
         hidden = set()
